8d1db559b75ed82a3a76e16873ecea477b857cd5
lsh9084 <sanghun1101088@gmail.com>
2024-11-24 16:54:04 +0900

    페이지네이션 및 배포테스트

--- src/pages/admin/User.jsx
+++ src/pages/admin/User.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 import { Modal } from '@/components/Modal'
 import AdminSidebar from '@/components/admin/AdminSidebar'
 import AdminHeader from '../../components/admin/AdminHeader'
+import Page from '../../components/Page'
 
 export default function AdminUser() {
     
@@ -116,31 +117,7 @@ export default function AdminUser() {
         <section className='flex justify-end gap-4 text-xs mb-10'>
             <button onClick={openOutsourcing} className='bg-blue white h-10 rounded-xl w-24'>외주업체 등록</button>
         </section>
-        <section className="flex justify-center mt-20">
-            <div className="flex items-center space-x-2">
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-600 hover:bg-gray-400 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
-                    <span className="hidden sm:inline">이전</span>
-                    <svg className="w-4 h-4 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-                        <path d="M15 19l-7-7 7-7"></path>
-                    </svg>
-                </button>
-                <button className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-lg text-sm font-semibold hover:from-blue-400 hover:to-indigo-400">
-                1
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-700 hover:bg-gray-400">
-                2
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-700 hover:bg-gray-400">
-                3
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-600 hover:bg-gray-400 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
-                    <span className="hidden sm:inline">다음</span>
-                    <svg className="w-4 h-4 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-                        <path d="M9 5l7 7-7 7"></path>
-                    </svg>
-                </button>
-            </div>
-        </section>
+        <Page />
         <section className='outsourcing-modal'>
             <Modal
                 isOpen={outsourcing}
@@ -263,31 +240,7 @@ export default function AdminUser() {
                 </tr>
             </tbody>
         </table>
-        <section className="flex justify-center mt-20">
-            <div className="flex items-center space-x-2">
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-600 hover:bg-gray-400 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
-                    <span className="hidden sm:inline">이전</span>
-                    <svg className="w-4 h-4 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-                        <path d="M15 19l-7-7 7-7"></path>
-                    </svg>
-                </button>
-                <button className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-lg text-sm font-semibold hover:from-blue-400 hover:to-indigo-400">
-                1
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-700 hover:bg-gray-400">
-                2
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-700 hover:bg-gray-400">
-                3
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-600 hover:bg-gray-400 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
-                    <span className="hidden sm:inline">다음</span>
-                    <svg className="w-4 h-4 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-                        <path d="M9 5l7 7-7 7"></path>
-                    </svg>
-                </button>
-            </div>
-        </section>
+        <Page />
       </>
       }
       </section>
