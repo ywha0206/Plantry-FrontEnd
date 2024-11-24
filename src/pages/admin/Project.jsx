03285ec6871bf40bdd438672653036c4a895a35f
lsh9084 <sanghun1101088@gmail.com>
2024-11-24 16:54:04 +0900

    페이지네이션 및 배포테스트

--- src/pages/admin/Project.jsx
+++ src/pages/admin/Project.jsx
@@ -7,6 +7,7 @@ import { Modal } from '../../components/Modal';
 import { useDispatch } from 'react-redux';
 import AdminSidebar from '../../components/admin/AdminSidebar';
 import AdminHeader from '../../components/admin/AdminHeader';
+import Page from '../../components/Page';
 
 export default function AdminProject() {
 
@@ -274,32 +275,8 @@ export default function AdminProject() {
             <button className='bg-blue white h-10 rounded-md w-24 hover:opacity-60'>선택 삭제</button>
             <button onClick={openMakeTask} className='bg-blue white h-10 rounded-md w-24 hover:opacity-60'>업무 등록</button>
         </section>
-        <section className="flex justify-center mt-20">
-            <div className="flex items-center space-x-2">
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-600 hover:bg-gray-400 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
-                    <span className="hidden sm:inline">이전</span>
-                    <svg className="w-4 h-4 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-                        <path d="M15 19l-7-7 7-7"></path>
-                    </svg>
-                </button>
-                <button className="px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-500 text-white rounded-lg text-sm font-semibold hover:from-blue-400 hover:to-indigo-400">
-                1
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-700 hover:bg-gray-400">
-                2
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-700 hover:bg-gray-400">
-                3
-                </button>
-                <button className="px-4 py-2 bg-gray-100 rounded-lg text-sm font-semibold text-gray-600 hover:bg-gray-400 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
-                    <span className="hidden sm:inline">다음</span>
-                    <svg className="w-4 h-4 sm:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-                        <path d="M9 5l7 7-7 7"></path>
-                    </svg>
-                </button>
-            </div>
-        </section>     
-            </>           
+        <Page />  
+        </>           
       }
       {selectOption === 2 &&
             <>
