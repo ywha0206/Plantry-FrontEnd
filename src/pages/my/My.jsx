7d2e53952bce7b8ef0f9db4cc353380b415f76e3
ywha0206 <ppyyhh3285@gmail.com>
2024-11-26 15:56:15 +0900

    mypage

--- src/pages/my/My.jsx
+++ src/pages/my/My.jsx
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '@/pages/my/My.scss'
+import MyAside from '@/components/my/MyAside.jsx';
+import { useNavigate, useParams } from "react-router-dom";
+
 
 export default function MyMain() {
+
+  const params = useParams();
+  const navigate = useNavigate();
+  const [progress, setProgress] = useState(65)
+  const [menuActive, setMenuActive] = useState("")
+
+  useEffect(() => {
+    if (params.page) {
+      setMenuActive(params.page); // e.g., 'my' from /my or other params
+    }else{
+      setMenuActive("")
+    }
+  }, [params.page]);
+  
+
+
+  
+  const profileModify = (e) => {
+    e.preventDefault();
+    navigate("/my/modify");
+  }
+
   return (
     <div id='my-main-container'>
+      <MyAside />
       <section className='my-main-main'>
-        
+        <article className='my-profile flex flex-col justify-center'>
+          <div className='flex flex-row justify-left items-center p-10 ml-[30px]'>
+            <img className='profile-img' src="/images/user_face_icon.png" alt="user-face-icon" />
+            <div className='flex flex-col ml-[40px]'>
+              <h3 className='text-lg mb-3 font-light text-gray-500'>yeonwha****</h3>
+              <div className='speech-bubble drop-shadow-lg py-[20px] px-[40px] flex items-center'>
+                <span>안녕하세요. 개발팀 박연화입니다.<br/> 어쩌고</span>
+              </div>
+            </div>
+          </div>
+          <div className='profile'>
+            <div className='myinfo border relative h-[450px] p-[20px] mr-10'>
+              <h2 className='text-lg mb-2 ml-2 text-gray-500 my-sub-title'>나의 프로필</h2>
+              <div >
+                <table className='myinfo-table mt-[30px]'>
+                  <tbody>
+                    <tr className='my-tr'>
+                      <th className='my-th'>이름  </th>
+                      <td className='my-td'>박연화</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>회사명 </th>
+                      <td className='my-td'>Plantry</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>부서 </th>
+                      <td className='my-td'>개발팀</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>직급 </th>
+                      <td className='my-td'>박연화</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>이메일 </th>
+                      <td className='my-td'>ppyyf@naver.com</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>전화번호 </th>
+                      <td className='my-td'>010-1234-1234</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>국가/지역 </th>
+                      <td className='my-td'>대한민국 / 부산</td>
+                    </tr>
+                    <tr className='my-tr'>
+                      <th className='my-th'>주소 </th>
+                      <td className='my-td'> 없음</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <button onClick={profileModify}
+                className='btn-profile bg-indigo-500 mt-20 float-right text-white  absolute bottom-[20px] right-[20px]'>프로필 수정</button>
+              </div>
+            </div>
+            <div className='current-plan relative border p-[20px]  h-[450px] '>
+              <h2 className='text-lg text-gray-600 my-sub-title'>나의 구독정보</h2>
+              <div className='flex justify-between items-center  mt-[30px]'>
+                <img className='plan-img' src="/images/current-plan-standard.png" alt="plan-icon" />
+                <div className='flex items-end'>
+                  <span className='text-indigo-500'>$</span>
+                  <h2 className='text-4xl text-indigo-500'>49</h2>
+                  <span className='font-extralight '>/month</span>
+                </div>
+              </div>
+              <ul className='ml-[20px] mt-[30px]'>
+                <li className='plan-exp'> 기본 플랜</li>
+                <li className='plan-exp'> 최대 100명 협업</li>
+                <li className='plan-exp'>10GB 드라이브 사용</li>
+              </ul>
+              <div>
+                <div className='flex justify-between mt-[30px]'>
+                  <span className='font-light'>Days</span>
+                  <span className='text-gray-500'>{`${progress}%`}</span>
+                </div>
+                <div className="progress-bar">
+                  <div className="progress-fill bg-indigo-400" style={{width: `${progress}%`}}></div>
+                </div>
+                <span className='text-sm text-gray-500'>4일 후 만료</span>
+              </div>
+                <button className='btn-profile  bg-indigo-500 text-white absolute bottom-[20px] right-[20px]'>결제정보등록</button>
+                <button className='btn-profile border border-indigo-500 text-indigo-800 absolute bottom-[20px] right-[193px]'>등록한 결제정보</button>
+              </div>
+          </div>
+        </article>
       </section>
     </div>
   )
---
+++ src/pages/my/MyModify.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from 'react'
+import '@/pages/my/My.scss'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function MyModify() {
+    const navigate = useNavigate();
+
+  const [deactive , setDeactive] = useState(0);
+  const handleCheckboxChange = (event) => {
+    setDeactive(event.target.checked ? 1 : 0); // 체크 상태에 따라 값 설정
+  };
+  const cancelHandler = (event) => {
+    event.preventDefault();
+    navigate("/my")
+  }
+
+  return <>
+    <div id='my-modify-container'>
+        <section className='my-modify1'>
+            {/* <h2 className='text-2xl font-light'>My profile</h2> */}
+            <form className='my-modify-form'>
+                <div className='upload-photo flex items-center'>
+                    <img className='user-img' src="/images/user_face_icon.png" alt="user-face" />
+                    <div className='relative top-5 left-10'>
+                        <input type="text" className='my-nick-inp mb-10 font-light text-gray-600' value="yeonwha****" />
+                        <div>
+                            <button className='btn-profile bg-indigo-500 text-white mr-10'>새 프로필 등록</button>
+                            <button className='btn-profile border border-red-400 text-red-400'>RESET</button>
+                        </div>
+                        <span className='text-sm text-gray-400 font-light'>JPG, GIF 혹은 PNG 등록 가능, 10MB 지원</span>
+                    </div>
+                </div>
+                <table className='w-full'>
+                    <tbody>
+                        <tr>
+                            <td className='flex mt-10'>
+                                <div className='flex flex-col w-1/2'>
+                                    <span className='text-xs text-gray-500 ml-10'>성</span>
+                                    <input type="text" className='border my-modify-input' />
+                                </div>
+                                <div className='flex flex-col w-1/2'>
+                                    <span className='text-xs text-gray-500 ml-10'>이름</span>
+                                    <input type="text" className='border my-modify-input' />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className='flex mt-10'>
+                                <div className='flex flex-col w-full'>
+                                    <span className='text-xs text-gray-500 ml-10'>이메일</span>
+                                    <div className='flex '>
+                                        <input type="text" className='border my-modify-input w-1/2' />
+                                        {/* <span className='h-full text-gray-500 text-2xl mr-10'>@</span>                             */}
+                                        <select className='my-modify-select text-gray-500 w-1/2'>
+                                            <option className='text-gray-500' value="">직접 입력</option>
+                                            <option className='text-gray-500' value="">@ naver.com</option>
+                                            <option className='text-gray-500' value="">@ gmail.com</option>
+                                            <option className='text-gray-500' value="">@ daum.net</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className='flex mt-10'>
+                                <div className='flex flex-col w-full'>
+                                    <span className='text-xs text-gray-500 ml-10'>전화번호</span>
+                                    <div className='flex items-center'>
+                                        <input type="text" className='border w-full my-modify-input' />
+                                        <span className='mr-10'>-</span><input type="text" className='border w-full my-modify-input' />
+                                        <span className='mr-10'>-</span><input type="text" className='border w-full my-modify-input' />
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className='flex mt-10'>
+                                <div className='flex flex-col w-1/2'>
+                                    <span className='text-xs text-gray-500 ml-10'>국가</span>
+                                    <input type="text" className='border my-modify-input' />
+                                </div>
+                                <div className='flex flex-col w-1/2'>
+                                    <span className='text-xs text-gray-500 ml-10'>거주지역</span>
+                                    <input type="text" className='border my-modify-input' />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className='flex mt-10'>
+                                <div className='flex flex-col w-full'>
+                                    <span className='text-xs text-gray-500 ml-10'>상세주소</span>
+                                    <input type="text" className='border my-modify-input' />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className='flex float-right mt-[50px]'>
+                    <button className='btn-profile border border-indigo-500 text-indigo-700 mr-10'>비밀번호 수정</button>
+                    <button className='btn-profile bg-indigo-500 text-white mr-10'>마이프로필 수정</button>
+                    <button className='btn-profile border text-gray-500' onClick={cancelHandler}>취소</button>
+                </div>
+            </form>
+        </section>
+        <section className='my-modify2'>
+            <article className='my-del-account flex flex-col justify-between'>
+                <h2 className='sub-title text-lg'>Delete Account</h2>
+                <div className='flex flex-col justify-start h-full'>
+                    <label><input type="checkbox" className='mt-[30px] mr-10' onChange={handleCheckboxChange}/> 계정을 비활성화하고 싶습니다.</label>
+                    {deactive===1&& 
+                    <div className='mt-[30px]'>
+                        <span className='text-xs ml-10'>먼저 비밀번호를 확인하셔야 합니다.</span>
+                        <div className='flex items-center '>
+                            <input type="password" className='delete-inp mr-10' />
+                            <button className='btn-delete bg-indigo-500 text-white' >확인</button>
+                        </div>
+                    </div>
+                    }
+                </div>
+                <button className='btn-profile border text-gray-500 w-full'>계정 비활성화</button>
+            </article>
+            <article className='my-social'>
+                <h2 className='sub-title text-lg'>Social Account</h2>
+                <span className='text-sm text-gray-500'>Plantry와 소셜 계정을 연동할 수 있습니다.</span>
+                <ul className='mt-[30px]'>
+                    <li className='flex justify-between items-center border p-2 rounded-lg mt-10'>
+                        <div className='flex'>
+                            <img src="/images/kakao-icon-box.png" alt="icon" className='icon-size-50 rounded-lg mr-10'/>
+                            <div>
+                                <p className='flex'>Kakao Talk <img className='ml-10' src="/images/Link-icon.png" alt="link" /></p>
+                                <span>연결된 계정의 이메일주소</span>
+                            </div>
+                        </div>
+                        <img src="/images/gold_star.png" alt="star" className='icon-size-25 mr-10'/>
+                    </li>
+                    <li className='flex justify-between items-center border p-2 rounded-lg mt-10'>
+                        <div className='flex'>
+                            <img src="/images/google-icon-box.png" alt="icon" className='icon-size-50 rounded-lg mr-10'/>
+                            <div>
+                                <p className='flex'>Google <img className='ml-10' src="/images/LinkOff-icon.png" alt="link" /></p>
+                                <span>연결된 계정의 이메일주소</span>
+                            </div>
+                        </div>
+                        <img src="/images/gray_star.png" alt="star" className='icon-size-25 mr-10'/>
+                    </li>
+                    <li className='flex justify-between items-center border p-2 rounded-lg mt-10'>
+                        <div className='flex'>
+                            <img src="/images/naver-icon-box.png" alt="icon" className='icon-size-50 rounded-lg mr-10'/>
+                            <div>
+                                <p className='flex'>Naver <img className='ml-10' src="/images/LinkOff-icon.png" alt="link" /></p>
+                                <span>연결된 계정의 이메일주소</span>
+                            </div>
+                        </div>
+                        <img src="/images/gray_star.png" alt="star" className='icon-size-25 mr-10'/>
+                    </li>
+                    <li className='flex justify-between items-center border p-2 rounded-lg mt-10'>
+                        <div className='flex'>
+                            <img src="/images/Facebook_icon-box.png" alt="icon" className='icon-size-50 rounded-lg mr-10'/>
+                            <div>
+                                <p className='flex'>Facebook <img className='ml-10' src="/images/LinkOff-icon.png" alt="link" /></p>
+                                <span>연결된 계정의 이메일주소</span>
+                            </div>
+                        </div>
+                        <img src="/images/gray_star.png" alt="star" className='icon-size-25 mr-10'/>
+                    </li>
+                </ul>
+            </article>
+        </section>
+    </div>
+  </>
+};
