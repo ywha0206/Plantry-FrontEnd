152ae41b7c8d4e9e583e0e6f91c2f1ccd2f413fa
SeoHong23 <ghd2408@naver.com>
2024-11-29 16:35:12 +0900

    홍합바보2

--- src/pages/community/View.jsx
+++ src/pages/community/View.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DiscussionEmbed } from "disqus-react";
 
-import CommunitySidebar from "@/components/community/CommunitySideBar";
+import CommunitySidebar from "@/components/community/CommunitySidebar";
 
 function CommunityView() {
   const navigate = useNavigate();
