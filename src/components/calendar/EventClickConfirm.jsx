2546ff459c9ec67cea8df898968daf40d42d235b
lsh9084 <sanghun1101088@gmail.com>
2024-12-01 23:17:31 +0900

    캘린더 기능구현

--- src/components/calendar/EventClickConfirm.jsx
+++ src/components/calendar/EventClickConfirm.jsx
@@ -10,8 +10,8 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
     const prevData = queryClient.getQueryData(['calendar-date']);
     const calendarNames = useCalenderNameStore((state) => state.calendarNames);
     const [selectedData, setSelectedData] = useState(() => {
-      const filteredData = prevData.filter(v => v.id == selectedId);
-      return filteredData.length > 0 ? filteredData[0] : {}; // 객체로 반환
+        const filteredData = prevData.filter(v => v.id == selectedId);
+        return filteredData.length > 0 ? filteredData[0] : {}; // 객체로 반환
     });
     const [calendarId, setCalendarId] = useState(() => {
         const selectedName = prevData.filter(v => v.id == selectedId);
@@ -27,24 +27,24 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
     const [customAlert, setCustomAlert] = useState(false);
     const [customAlertType, setCustomAlertType] = useState("");
     const [customAlertMessage, setCustomAlertMessage] = useState("");
-    
-    
+
+
 
     useEffect(()=>{
-      if(selectedData){
-        setTitle(selectedData.title)
-        setSdate(selectedData.start)
-        setEdate(selectedData.end)
-        setLocation(selectedData.location)
-        setImportance(selectedData.importance)
-        setAlert(selectedData.alert)
-        setMemo(selectedData.memo)
-      }
-      console.log(prevData)
+        if(selectedData){
+            setTitle(selectedData.title)
+            setSdate(selectedData.start)
+            setEdate(selectedData.end)
+            setLocation(selectedData.location)
+            setImportance(selectedData.importance)
+            setAlert(selectedData.alert)
+            setMemo(selectedData.memo)
+        }
+        console.log(prevData)
     },[selectedData])
 
     const deleteMutation = useMutation({
-        
+
         mutationFn : async () => {
             const response = await axiosInstance.delete(`/api/calendar/content?id=${selectedId}`, {
 
@@ -56,7 +56,7 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
             setCustomAlertType("success")
             setCustomAlertMessage(data.message)
             queryClient.setQueryData(['calendar-date'], (prevData) => {
-                return prevData.filter((v) => v.id !== data.id); 
+                return prevData.filter((v) => v.id !== data.id);
             });
             setTimeout(()=>{
                 setCustomAlert(false);
@@ -91,18 +91,18 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
 
     const putCalendarMutation = useMutation({
         mutationFn : async () => {
-          const response = await axiosInstance.put("/api/calendar/content",{
-            title,
-            sdate,
-            edate,
-            calendarId : selectedId,
-            sheave : calendarId,
-            location,
-            importance,
-            alert,
-            memo,
-          })
-          return response.data
+            const response = await axiosInstance.put("/api/calendar/content",{
+                title,
+                sdate,
+                edate,
+                calendarId : selectedId,
+                sheave : calendarId,
+                location,
+                importance,
+                alert,
+                memo,
+            })
+            return response.data
         },
         onError: (err) => {
             setCustomAlert(true)
@@ -120,8 +120,8 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
                     if (v.id == selectedId) {
                         return {
                             ...v,
-                            title: title, 
-                            start: sdate, 
+                            title: title,
+                            start: sdate,
                             end: edate,
                             location: location,
                             alert: alert,
@@ -131,9 +131,9 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
                             color: data.color
                         };
                     }
-                    return v; 
+                    return v;
                 });
-            
+
                 return updatedData; // 새로운 배열 반환
             });
             setCustomAlert(true)
@@ -145,11 +145,11 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
                 onclose();
             },1000)
         }
-      })
-  
-      const putCalendarHandler = async () =>{
+    })
+
+    const putCalendarHandler = async () =>{
         try {
-          await putCalendarMutation.mutateAsync(); 
+            await putCalendarMutation.mutateAsync();
         } catch (err){
             setCustomAlert(true)
             setCustomAlertType("error")
@@ -159,111 +159,107 @@ export default function EventClickConfirm({isOpen, onclose,selectedId,clickedDat
                 setCustomAlert(false)
             },1000)
         }
-      }
+    }
 
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 modal-custom-fixed">
-          <div className="bg-white z-40 rounded-2xl shadow-lg w-[800px] modal-custom-width max-h-[800px] overflow-scroll scrollbar-none">
-            <div className="display-flex mb-8 py-3.5 px-12 bg-white border-b rounded-t-2xl z-10 sticky top-0">
-                <span className="text-xl font-bold">일정 상세보기</span>
-                <button 
-                onClick={onclose}
-                className="text-xl float-right display-block font-bold text-gray-600 hover:text-gray-900"
-                >
-                X
-                </button>
+            <div className="bg-white z-40 rounded-2xl shadow-lg w-[800px] modal-custom-width max-h-[800px] overflow-scroll scrollbar-none">
+                <div className="display-flex mb-8 py-3.5 px-12 bg-white border-b rounded-t-2xl z-10 sticky top-0">
+                    <span className="text-xl font-bold">일정 상세보기</span>
+                    <button
+                        onClick={onclose}
+                        className="text-xl float-right display-block font-bold text-gray-600 hover:text-gray-900"
+                    >
+                        X
+                    </button>
+                </div>
+                <div className=" mx-12">
+                    <div className="flex gap-8 mb-4 justify-start">
+                        <span className="w-20 h-[40px]">제목</span>
+                        <div>
+                            <input onChange={(e)=>setTitle(e.target.value)} value={title} className="h-[40px] w-[250px] border rounded-md px-2"></input>
+                        </div>
+                    </div>
+                    <div className="flex gap-8 justify-start mb-4">
+                        <span className="w-20 h-[40px]">날짜/시간</span>
+                        <div className="flex gap-3 items-center">
+                            <input value={sdate} onChange={(e)=>setSdate(new Date(e.target.value).toLocaleString('sv-SE'))} className='h-[40px] w-[200px]' type="datetime-local"></input> ~ <input value={edate} onChange={(e)=>setEdate(new Date(e.target.value).toLocaleString('sv-SE'))} className='h-[40px] w-[200px]' type="datetime-local"></input>
+                        </div>
+                    </div>
+                    <div className="flex gap-8 mb-4 justify-start">
+                        <span className="w-20 h-[40px]">켈린더</span>
+                        <div>
+                            {
+                                (!Array.isArray(calendarNames) || calendarNames.length === 0)
+                                    ?
+                                    (
+                                        <ul>등록된 캘린더가 없습니다...</ul>
+                                    ) :
+                                    (
+                                        <ul>
+                                            <select value={calendarId} onChange={(e)=>setCalendarId(e.target.value)} className="h-[40px] w-[120px] outline-none border rounded-md text-center">
+                                                <option value={0}>달력선택</option>
+                                                {calendarNames.map(v=>{
+                                                    return (
+                                                        <option key={v.id} value={Number(v.id)}>{v.name}</option>
+
+                                                    )
+                                                })}
+                                            </select>
+                                        </ul>
+                                    )
+                            }
+
+                        </div>
+                    </div>
+                    <div className="flex gap-8 mb-4 justify-start">
+                        <span className="w-20 h-[40px]">장소</span>
+                        <div>
+                            <input value={location} onChange={(e)=>setLocation(e.target.value)} className="h-[40px] w-[200px] border rounded-md px-2"></input>
+                        </div>
+                    </div>
+                    <div className="flex gap-8 mb-4 justify-start">
+                        <span className="w-20 h-[40px]">중요도</span>
+                        <div>
+                            <select value={importance} onChange={(e)=> setImportance(e.target.value)} className="h-[40px] text-center w-[100px] outline-none border rounded-md">
+                                <option value={1}>낮음</option>
+                                <option value={2}>보통</option>
+                                <option value={3}>높음</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex gap-8 mb-4 justify-start">
+                        <span className="w-20 h-[40px]">알림</span>
+                        <div>
+                            <select value={alert} onChange={(e)=>setAlert(e.target.value)} className="h-[40px] text-center w-[100px] outline-none border rounded-md">
+                                <option value={1}>낮음</option>
+                                <option value={2}>보통</option>
+                                <option value={3}>높음</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex gap-8 mb-8 justify-start">
+                        <span className="w-20 h-[30px]">메모</span>
+                        <div>
+                            <textarea value={memo} onChange={(e)=>setMemo(e.target.value)} className="h-[120px] w-[380px] border rounded-md resize-none px-2"></textarea>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='flex py-8 px-8 justify-end gap-4 items-center'>
+                    <button onClick={putCalendarHandler} className='bg-purple w-[110px] hover:opacity-80 py-4 text-sm rounded-md white'>수정하기</button>
+                    <button
+                        className="bg-purple w-[110px] py-4 text-sm rounded-md white hover:opacity-60 cursor-pointer"
+                        onClick={deleteCalendar}
+                    >
+                        삭제
+                    </button>
+                </div>
             </div>
-            <div className=" mx-12">
-              <div className="flex gap-8 mb-4 justify-start">
-                <span className="w-20 h-[40px]">제목</span>
-                <div>
-                  <input onChange={(e)=>setTitle(e.target.value)} value={title} className="h-[40px] w-[250px] border rounded-md px-2"></input>
-                </div>
-              </div>
-              <div className="flex gap-8 justify-start mb-4">
-                <span className="w-20 h-[40px]">날짜/시간</span>
-                <div className="flex gap-3 items-center">
-                  <input value={sdate} onChange={(e)=>setSdate(new Date(e.target.value).toLocaleString('sv-SE'))} className='h-[40px] w-[200px]' type="datetime-local"></input> ~ <input value={edate} onChange={(e)=>setEdate(new Date(e.target.value).toLocaleString('sv-SE'))} className='h-[40px] w-[200px]' type="datetime-local"></input>
-                </div>
-              </div>
-              <div className="flex gap-8 mb-4 justify-start">
-                <span className="w-20 h-[40px]">켈린더</span>
-                <div>
-                    {
-                      (!Array.isArray(calendarNames) || calendarNames.length === 0)
-                      ?
-                      (
-                      <ul>등록된 캘린더가 없습니다...</ul>
-                      ) :
-                      (
-                      <ul>
-                        <select value={calendarId} onChange={(e)=>setCalendarId(e.target.value)} className="h-[40px] w-[120px] outline-none border rounded-md text-center">
-                          <option value={0}>달력선택</option>
-                          {calendarNames.map(v=>{
-                          return (
-                              <option key={v.id} value={Number(v.id)}>{v.name}</option>
-        
-                          )
-                          })}
-                          </select>
-                      </ul>
-                      )
-                    }
-                  
-                </div>
-              </div>
-              <div className="flex gap-8 mb-4 justify-start">
-                <span className="w-20 h-[40px]">장소</span>
-                <div>
-                  <input value={location} onChange={(e)=>setLocation(e.target.value)} className="h-[40px] w-[200px] border rounded-md px-2"></input>
-                </div>
-              </div>
-              <div className="flex gap-8 mb-4 justify-start">
-                <span className="w-20 h-[40px]">중요도</span>
-                <div>
-                  <select value={importance} onChange={(e)=> setImportance(e.target.value)} className="h-[40px] text-center w-[100px] outline-none border rounded-md">
-                    <option value={1}>낮음</option>
-                    <option value={2}>보통</option>
-                    <option value={3}>높음</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex gap-8 mb-4 justify-start">
-                <span className="w-20 h-[40px]">알림</span>
-                <div>
-                  <select value={alert} onChange={(e)=>setAlert(e.target.value)} className="h-[40px] text-center w-[100px] outline-none border rounded-md">
-                    <option value={1}>낮음</option>
-                    <option value={2}>보통</option>
-                    <option value={3}>높음</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex gap-8 mb-8 justify-start">
-                <span className="w-20 h-[30px]">메모</span>
-                <div>
-                  <textarea value={memo} onChange={(e)=>setMemo(e.target.value)} className="h-[120px] w-[380px] border rounded-md resize-none px-2"></textarea>
-                </div>
-              </div>
-        
-            </div>
-<<<<<<< HEAD
-            <div className='flex py-8 px-8 justify-end gap-4 items-center'>
-=======
-            <div className='flex py-8 px-8 justify-between items-center'>
->>>>>>> 98f236428002da84caccc043697ce76292703f8f
-                <button onClick={putCalendarHandler} className='bg-purple w-[110px] hover:opacity-80 py-4 text-sm rounded-md white'>수정하기</button>
-                <button 
-                className="bg-purple w-[110px] py-4 text-sm rounded-md white hover:opacity-60 cursor-pointer"
-                onClick={deleteCalendar}
-                >
-                삭제
-                </button>
-            </div>
-          </div>
-          <CustomAlert 
-              type={customAlertType} message={customAlertMessage} isOpen={customAlert}
-          />
+            <CustomAlert
+                type={customAlertType} message={customAlertMessage} isOpen={customAlert}
+            />
         </div>
-      )
+    )
 }
