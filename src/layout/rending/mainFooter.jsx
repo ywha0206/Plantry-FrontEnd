12ca7a307c4a8d4fca4306e93b5fae6acfbf0ccf
seungwoosin <soc23231@gmail.com>
2024-12-06 13:28:45 +0900

    랜딩페이지

--- src/layout/rending/mainFooter.jsx
+++ src/layout/rending/mainFooter.jsx
@@ -5,19 +5,14 @@ export default function MainFooter(){
         <footer>
             <div className="footerIn">
                 <section className="service">
-                    <h4><Link to="/service">서비스 소개</Link></h4>
+                    <h4><Link to="/service">서비스</Link></h4>
                     <p><Link to="">UI design</Link></p>
                     <p><Link to="">UX design</Link></p>
                 </section>
                 <section className="selling">
-                    <h4><Link to="">가격 및 혜택</Link></h4>
+                    <h4><Link to="">요금소개</Link></h4>
                     <p><Link to="">가격</Link></p>
                     <p><Link to="">혜택</Link></p>
-                </section>
-                <section className="experience">
-                    <h4><Link to="">체험 및 도입</Link></h4>
-                    <p><Link to="">체험</Link></p>
-                    <p><Link to="">도입 해보기</Link></p>
                 </section>
                 <section className="cscenter">
                     <h4><Link to="">고객센터</Link></h4>
--- src/layout/rending/mainHeader.jsx
+++ src/layout/rending/mainHeader.jsx
@@ -50,13 +50,13 @@ export default function MainHeader() {
 
           <div className="flex items-center space-x-4">
             <Link
-              to="/login"
+              to="/user/login"  // 로그인 페이지 경로 수정
               className="px-4 py-2 border border-gray-400 rounded text-gray-700 hover:bg-[#666bff] hover:text-white"
             >
               로그인
             </Link>
             <Link
-              to="/join"
+              to="/user/terms"
               className="px-4 py-2 border border-gray-400 rounded text-gray-700 hover:bg-[#666bff] hover:text-white"
             >
               회원가입
