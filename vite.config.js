58214f25d761fcb5526fdbcb0aadbe9e99d77394
ywha0206 <ppyyhh3285@gmail.com>
2024-12-24 19:10:10 +0900

    plz

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import ViteNodePolyfills from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), ViteNodePolyfills()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
